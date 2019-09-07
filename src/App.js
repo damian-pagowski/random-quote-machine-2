4a486b912b4e5753c98d85f461c6be0ccc6c131d
Damian <d.pagowski@gmail.com>
2019-09-07 19:19:02 +0700

    UI implemented

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
 
-function App() {
+function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div class='section'>
+        <div id='quote-box'>
+          <h1>Quote Of The Day</h1>
+
+          <p class='lead'>
+            <i class='fa fa-quote-left' /> <span id="text">Lorem ipsum dolor sit, amet
+            consectetur adipisicing elit. Commodi, quis! </span>
+          </p>
+
+          <div class='quote-author' id="author">- Steve Jobs</div>
+          <div class='social-media'>
+            <a id="" href='#' class='fa fa-facebook' />
+            <a id="tweet-quote" href='twitter.com/intent/tweet' class='fa fa-twitter' />
+          </div>
+          <button id="new-quote" type='button' class='btn btn-outline-dark text-light'>
+            Get Another
+          </button>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
